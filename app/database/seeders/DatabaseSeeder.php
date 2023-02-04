077936c2bb1cdc2f7b28dbaa63c9687ae7a6d00f
Jovana Lazarevic <jovanalazarevic394@gmail.com>
2023-02-04 16:50:18 +0100

    Definisan seeder

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Bioskop;
+use \App\Models\Film;
+use \App\Models\Ocena;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +19,55 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
+        //brisanje
+        User::truncate();
+        Bioskop::truncate();
+        Film::truncate();
+        Ocena::truncate();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory(5)->create();
+        Bioskop::factory(10)->create();
+
+        $film1 = Film::create([
+            'name' => 'Avatar'
+        ]);
+
+        $film2 = Film::create([
+            'name' => 'Oluja'
+        ]);
+
+        $film3 = Film::create([
+            'name' => 'Čovek po imenu Oto'
+        ]);
+
+        $film4 = Film::create([
+            'name' => 'Alkaras'
+        ]);
+
+        $film5 = Film::create([
+            'name' => 'Opasan let'
+        ]);
+
+        $ocena1 = Ocena::create([
+            'datum_i_vreme' => now(),
+            'korisnik' => 1,
+            'film' => 1,
+            'bioskop' => 1,
+            'ocena' => 5,
+            'poruka' => 'Avanturističan, odlična gluma.'
+        ]);
+
+        $ocena2 = Ocena::create([
+            'datum_i_vreme' => now(),
+            'korisnik' => 2,
+            'film' => 2,
+            'bioskop' => 2,
+            'ocena' => 5,
+            'poruka' => 'Odlična gluma, prijatno osoblje.'
+        ]);
     }
 }
