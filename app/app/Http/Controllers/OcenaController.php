13f170666c71e9dfe7ea9841822d25829c41839b
Jovana Lazarevic <jovanalazarevic394@gmail.com>
2023-02-04 16:59:41 +0100

    Napravljeni Kontroleri

---
+++ app/app/Http/Controllers/OcenaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ocena;
+use Illuminate\Http\Request;
+
+class OcenaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ocena  $ocena
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ocena $ocena)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ocena  $ocena
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ocena $ocena)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ocena  $ocena
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ocena $ocena)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ocena  $ocena
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ocena $ocena)
+    {
+        //
+    }
+}
