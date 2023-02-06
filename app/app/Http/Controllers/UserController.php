a53c05a2b5db5995a3130c282de535f5e9a9256e
Jovana Lazarevic <jovanalazarevic394@gmail.com>
2023-02-06 10:16:46 +0100

    Implementirane funkcije u UserController-u

--- app/app/Http/Controllers/UserController.php
+++ app/app/Http/Controllers/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserCollection;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 
 class UserController extends Controller
@@ -16,7 +20,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        if (auth()->user()->isAdmin())
+            return new UserCollection(User::all());
     }
 
     /**
@@ -48,7 +53,8 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
+        if (auth()->user()->isAdmin())
+            return new UserResource($user);
     }
 
     /**
@@ -71,8 +77,29 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        if (auth()->user()->id == $user->id || auth()->user()->isAdmin()) {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:100',
+                'email' => 'required|max:50|email|unique:users,email,' . $user->id,
+                'password' => 'required|string|regex:"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"'
+            ]);
+
+            if ($validator->fails())
+                return response()->json($validator->errors());
+
+            $user->email_verified_at = now();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+
+            $user->save();
+
+            return response()->json(['user' => $user, 'access_token' => $user->remember_token, 'token_type' => 'Bearer']);
+        }
+
+        return response()->json('Niste ovlašćeni da vršite izmene');
     }
+
 
     /**
      * Remove the specified resource from storage.
@@ -82,6 +109,11 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        if ($user->id != auth()->user()->id && auth()->user()->isAdmin()) {
+            $user->delete();
+            return response()->json('Korisnik uspešno obrisan.');
+        }
+
+        return response()->json('Ne možete da obrišete sebe ili drugog korisnika, ukoliko niste admin.');
     }
 }
